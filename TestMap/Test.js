338b7236f6cf9210d38bfb7b275dff7beddfff5e
mflynn94 <mflynn94@hotmail.co.uk>
2020-05-17 17:35:12 +0100

    Add files via upload

---
+++ TestMap/Test.js
@@ -0,0 +1,12 @@
+var geojsonFeature = {
+        "type": "Feature",
+        "properties": {
+            "name": "Coors Field",
+            "amenity": "Baseball Stadium",
+            "popupContent": "This is where the Rockies play!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-4, 55]
+        }
+    };
