d3e90026ba38f7b5b0c6017d6e4fc8f3092b556c
mflynn94 <mflynn94@hotmail.co.uk>
2020-05-17 17:27:50 +0100

    Add files via upload

---
+++ TestMap/Processed_Pharmacies_Test.js
@@ -0,0 +1,119 @@
+var Pharmacies = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "DispenserName": "ABERFELDY AND KINLOCH RANNOCH MEDICAL PRACTICE",
+        "Address1": "ABERFELDY AND KINLOCH RANNOCH MEDICAL PRACTICE",
+        "Address2": "TAYBRIDGE ROAD",
+        "Address3": "ABERFELDY",
+        "Address4": "PERTHSHIRE",
+        "Postcode": "PH152BL",
+        "Telephone": "01887 820366"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -3.8705,
+          56.6183
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "DispenserName": "A G BANNERMAN LTD",
+        "Address1": "220/222 SARACEN STREET",
+        "Address2": "GLASGOW",
+        "Address3": NaN,
+        "Address4": NaN,
+        "Postcode": "G22 5ER",
+        "Telephone": "0141336 8114"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.2539,
+          55.8832
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "DispenserName": "ASDA STORES LIMITED",
+        "Address1": "PHARMACY DEPARTMENT",
+        "Address2": "MONUMENT DRIVE",
+        "Address3": "GLASGOW",
+        "Address4": NaN,
+        "Postcode": "G33 1AD",
+        "Telephone": "0141 557 6210"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.1825,
+          55.888999999999996
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "DispenserName": "LLOYDS PHARMACY LTD",
+        "Address1": "26 BRIDGETON CROSS",
+        "Address2": "GLASGOW",
+        "Address3": NaN,
+        "Address4": NaN,
+        "Postcode": "G40 1BW",
+        "Telephone": "0141554 2643"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.2269,
+          55.849
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "DispenserName": "LLOYDS PHARMACY LTD",
+        "Address1": "426 CARMUNNOCK ROAD",
+        "Address2": "GLASGOW",
+        "Address3": NaN,
+        "Address4": NaN,
+        "Postcode": "G44 5EH",
+        "Telephone": "0141637 0377"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.2451,
+          55.8107
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "DispenserName": "LLOYDS PHARMACY LTD",
+        "Address1": "375 KILBOWIE ROAD",
+        "Address2": "CLYDEBANK",
+        "Address3": NaN,
+        "Address4": NaN,
+        "Postcode": "G81 2TU",
+        "Telephone": "0141 952 9510"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.4035,
+          55.9101
+        ]
+      }
+    }
+  ]
+};
